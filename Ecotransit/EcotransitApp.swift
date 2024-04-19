f93b7c6e2e8f8a45b81687ae0bc9d1c696eff30f
Jorge_Jimenez <112440747+jorge-ivan-jimenez-reyes@users.noreply.github.com>
2024-04-18 21:30:59 -0600

    Initial Commit

---
+++ Ecotransit/EcotransitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcotransitApp.swift
+//  Ecotransit
+//
+//  Created by Jorge Ivan JImenez Reyes on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EcotransitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
